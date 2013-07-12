e0a3c1a9ada8fd0f8df8655f427bbcdf9455fb52
Peter Zhang <info@zhtech.com>
2013-07-12 04:09:32 +0000

    added index.html and modified web.js to read content from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
+var fs = require('fs');
+
+var buffer = new Buffer(fs.readFileSync('index.html'), 'utf-8');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
